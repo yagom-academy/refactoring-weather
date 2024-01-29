951aedd82536bcd06a8b21087c1fc7cf8cd4ad9c
Kodirbek Khamzaev <qodirbekkhamzaev@gmail.com>
2024-01-29 23:11:27 +0900

    fix: initial rearrangement of VC

--- WeatherForecast/WeatherForecast/ViewController.swift
+++ WeatherForecast/WeatherForecast/ViewController.swift
@@ -1,17 +1,23 @@
 //
 //  WeatherForecast - ViewController.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: - Properties
+    var icons: [UIImage]?
+    var weatherJSON: WeatherJSON?
+    let imageChache: NSCache<NSString, UIImage> = NSCache()
+    var tempUnit: TempUnit = .metric
+    
+    // MARK: - UI
     var tableView: UITableView!
     let refreshControl: UIRefreshControl = UIRefreshControl()
-    var weatherJSON: WeatherJSON?
-    var icons: [UIImage]?
-    let imageChache: NSCache<NSString, UIImage> = NSCache()
+    
     let dateFormatter: DateFormatter = {
         let formatter: DateFormatter = DateFormatter()
         formatter.locale = .init(identifier: "ko_KR")
@@ -19,23 +25,47 @@ class ViewController: UIViewController {
         return formatter
     }()
     
-    var tempUnit: TempUnit = .metric
-    
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        initialSetUp()
+        layoutTableView()
+        setUpTableView()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "화씨", image: nil, target: self, action: #selector(changeTempUnit))
     }
-}
-
-extension ViewController {
+    
+    // MARK: - SetupUI
+    private func setUpTableView() {
+        refreshControl.addTarget(self, action: #selector(refresh), for: .valueChanged)
+        tableView.refreshControl = refreshControl
+        
+        tableView.register(WeatherTableViewCell.self, forCellReuseIdentifier: "WeatherCell")
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    
+    private func layoutTableView() {
+        let safeArea: UILayoutGuide = view.safeAreaLayoutGuide
+        tableView = .init(frame: .zero, style: .plain)
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+        ])
+    }
+    
+    // MARK: - Methods
     @objc private func changeTempUnit() {
         switch tempUnit {
-        case .imperial:
-            tempUnit = .metric
-            navigationItem.rightBarButtonItem?.title = "섭씨"
-        case .metric:
-            tempUnit = .imperial
-            navigationItem.rightBarButtonItem?.title = "화씨"
+            case .imperial:
+                tempUnit = .metric
+                navigationItem.rightBarButtonItem?.title = "섭씨"
+            case .metric:
+                tempUnit = .imperial
+                navigationItem.rightBarButtonItem?.title = "화씨"
         }
         refresh()
     }
@@ -46,43 +76,16 @@ extension ViewController {
         refreshControl.endRefreshing()
     }
     
-    private func initialSetUp() {
-        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "화씨", image: nil, target: self, action: #selector(changeTempUnit))
-        
-        layTable()
-        
-        refreshControl.addTarget(self,
-                                 action: #selector(refresh),
-                                 for: .valueChanged)
-        
-        tableView.refreshControl = refreshControl
-        tableView.register(WeatherTableViewCell.self, forCellReuseIdentifier: "WeatherCell")
-        tableView.dataSource = self
-        tableView.delegate = self
-    }
     
-    private func layTable() {
-        tableView = .init(frame: .zero, style: .plain)
-        view.addSubview(tableView)
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let safeArea: UILayoutGuide = view.safeAreaLayoutGuide
-        
-        NSLayoutConstraint.activate([
-            tableView.topAnchor.constraint(equalTo: safeArea.topAnchor),
-            tableView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
-            tableView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
-        ])
-    }
 }
 
+// MARK: - Extension: Data
 extension ViewController {
     private func fetchWeatherJSON() {
         
         let jsonDecoder: JSONDecoder = .init()
         jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
-
+        
         guard let data = NSDataAsset(name: "weather")?.data else {
             return
         }
@@ -94,12 +97,13 @@ extension ViewController {
             print(error.localizedDescription)
             return
         }
-
+        
         weatherJSON = info
         navigationItem.title = weatherJSON?.city.name
     }
 }
 
+// MARK: - Extension: UITableViewDataSource
 extension ViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
@@ -124,10 +128,10 @@ extension ViewController: UITableViewDataSource {
         
         let date: Date = Date(timeIntervalSince1970: weatherForecastInfo.dt)
         cell.dateLabel.text = dateFormatter.string(from: date)
-                
-        let iconName: String = weatherForecastInfo.weather.icon         
+        
+        let iconName: String = weatherForecastInfo.weather.icon
         let urlString: String = "https://openweathermap.org/img/wn/\(iconName)@2x.png"
-                
+        
         if let image = imageChache.object(forKey: urlString as NSString) {
             cell.weatherIcon.image = image
             return cell
@@ -151,6 +155,7 @@ extension ViewController: UITableViewDataSource {
     }
 }
 
+// MARK: - Extension: UITableViewDelegate
 extension ViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)

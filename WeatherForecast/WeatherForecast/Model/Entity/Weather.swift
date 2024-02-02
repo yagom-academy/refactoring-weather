1e35799b02fbc1a677c1cfdc8e7e03908212c894
ChangMin <sopg04@gmail.com>
2024-02-03 03:38:20 +0900

    STEP 2 : Entity에 접근지정자 추가

--- WeatherForecast/WeatherForecast/Model/Entity/Weather.swift
+++ WeatherForecast/WeatherForecast/Model/Entity/Weather.swift
@@ -9,43 +9,43 @@ import Foundation
 
 // MARK: - Weather JSON Format
 struct Weather {
-    let weatherForecast: [WeatherForecastInfo]
-    let city: CityInfo
+    private(set) var weatherForecast: [WeatherForecastInfo]
+    private(set) var city: CityInfo
 }
 
 // MARK: - List
 struct WeatherForecastInfo {
-    let dt: TimeInterval
-    let main: MainInfo
-    let weather: WeatherInfo
-    let dtTxt: String
+    private(set) var dt: TimeInterval
+    private(set) var main: MainInfo
+    private(set) var weather: WeatherInfo
+    private(set) var dtTxt: String
 }
 
 // MARK: - MainClass
 struct MainInfo {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, seaLevel, grndLevel, humidity, pop: Double
+    private(set) var temp, feelsLike, tempMin, tempMax: Double
+    private(set) var pressure, seaLevel, grndLevel, humidity, pop: Double
 }
 
 // MARK: - Weather
 struct WeatherInfo {
-    let id: Int
-    let main: String
-    let description: String
-    let icon: String
+    private(set) var id: Int
+    private(set) var main: String
+    private(set) var description: String
+    private(set) var icon: String
 }
 
 // MARK: - City
 struct CityInfo {
-    let id: Int
-    let name: String
-    let coord: Coord
-    let country: String
-    let population, timezone: Int
-    let sunrise, sunset: TimeInterval
+    private(set) var id: Int
+    private(set) var name: String
+    private(set) var coord: Coord
+    private(set) var country: String
+    private(set) var population, timezone: Int
+    private(set) var sunrise, sunset: TimeInterval
 }
 
 // MARK: - Coord
 struct Coord {
-    let lat, lon: Double
+    private(set) var lat, lon: Double
 }

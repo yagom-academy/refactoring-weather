b4dd26e4e34f6d7d8a54faee697e27542548fe52
Hyosung <hyosung@finda.co.kr>
2024-02-03 19:47:38 +0900

    feature: URLSession을 Background 로 돌리는 코드 추가

--- WeatherForecast/WeatherForecast/Model/Implement/ImageDownLoader.swift
+++ WeatherForecast/WeatherForecast/Model/Implement/ImageDownLoader.swift
@@ -8,17 +8,36 @@
 import UIKit
 
 struct ImageDownLoader: ImageDownLoaderService {
+  @MainActor
   func image(url: String) async -> Result<UIImage, Error> {
     var result: Result<UIImage, Error>
     
     guard let url = URL(string: url),
-          let (data, _) = try? await URLSession.shared.data(from: url),
-          let image = UIImage(data: data) else {
+          let (data, _) = try? await URLSession.shared.data(from: url)
+    else {
       result = .failure(NSError())
       return result
     }
     
-    result = .success(image)
+    var image: UIImage?
+    
+    await withTaskGroup(of: UIImage?.self) { imageTaskGroup in
+      imageTaskGroup.addTask(
+        priority: .background) {
+          return UIImage(data: data)
+        }
+      
+      for await loadedImage in imageTaskGroup {
+        image = loadedImage
+      }
+    }
+    
+    if let image = image {
+      result = .success(image)
+    } else {
+      result = .failure(NSError())
+    }
+    
     return result
   }
 }

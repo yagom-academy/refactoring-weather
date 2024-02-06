57361aab8bd3d256a0f11ef53bb26a8ba880a379
Beepeach <eh7457303j@gmail.com>
2024-02-06 21:50:37 +0900

    Feat: ImageCache 모델 추가
    UIImage를 UseCase에서 가지게 되면 UIKit을 import해야하고
    NSData로 저장하게되면 Data로 타입캐스팅을 해야해서 새로운 모델을 만들었습니다.

---
+++ WeatherForecast/WeatherForecast/Models/ImageCache.swift
@@ -0,0 +1,16 @@
+//
+//  ImageCache.swift
+//  WeatherForecast
+//
+//  Created by JunHeeJo on 2/6/24.
+//
+
+import Foundation
+
+class ImageCache {
+    let data: Data
+    
+    init(data: Data) {
+        self.data = data
+    }
+}

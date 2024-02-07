5d3d74bd657f8fc132d80ce6af71acf5eaba9a3f
Beepeach <eh7457303j@gmail.com>
2024-02-07 18:55:55 +0900

    Feat: WeatherListUseCase에서 이미지를 캐싱하고 가져오는 메서드 추가

--- WeatherForecast/WeatherForecast/UseCases/WeatherListUseCase.swift
+++ WeatherForecast/WeatherForecast/UseCases/WeatherListUseCase.swift
@@ -9,9 +9,20 @@ import Foundation
 
 protocol WeatherListUseCase {
     func fetchWeatherList(url: URL?) -> CityWeather?
+    func fetchWeatherImage(url: String) async -> ImageCache?
 }
 
 final class DefaultWeatherListUseCase: WeatherListUseCase {
+    private var imageCacheService: ImageChacheService
+    
+    init(imageCacheService: ImageChacheService) {
+        self.imageCacheService = imageCacheService
+    }
+    
+    convenience init() {
+        self.init(imageCacheService: DefaultImageChacheService.shared)
+    }
+    
     func fetchWeatherList(url: URL?) -> CityWeather? {
         let jsonDecoder: JSONDecoder = JSONDecoderCreator.createSnakeCaseDecoder()
 
@@ -27,5 +38,9 @@ final class DefaultWeatherListUseCase: WeatherListUseCase {
         }
         
         return info
+    }
+    
+    func fetchWeatherImage(url: String) async -> ImageCache? {
+        await imageCacheService.fetchImage(urlString: url)
     }
 }

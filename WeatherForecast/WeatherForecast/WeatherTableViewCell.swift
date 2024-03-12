8fda2aac1ee099f374dc9d5218555b01a279de05
honghoker <ekflzhftk@naver.com>
2024-03-12 16:15:28 +0900

    STEP 1: 프로젝트 전체의 코드 indent를 4에서 2로 변경

--- WeatherForecast/WeatherForecast/WeatherTableViewCell.swift
+++ WeatherForecast/WeatherForecast/WeatherTableViewCell.swift
@@ -1,103 +1,103 @@
 //
 //  WeatherForecast - WeatherTableViewCell.swift
-//  Created by yagom. 
+//  Created by yagom.
 //  Copyright © yagom. All rights reserved.
-// 
+//
 
 import UIKit
 
 class WeatherTableViewCell: UITableViewCell {
-    var weatherIcon: UIImageView!
-    var dateLabel: UILabel!
-    var temperatureLabel: UILabel!
-    var weatherLabel: UILabel!
-    var descriptionLabel: UILabel!
-     
-    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
-        super.init(style: style, reuseIdentifier: reuseIdentifier)
-        layViews()
-        reset()
+  var weatherIcon: UIImageView!
+  var dateLabel: UILabel!
+  var temperatureLabel: UILabel!
+  var weatherLabel: UILabel!
+  var descriptionLabel: UILabel!
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+    layViews()
+    reset()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError()
+  }
+  
+  override func prepareForReuse() {
+    super.prepareForReuse()
+    reset()
+  }
+  
+  private func layViews() {
+    weatherIcon = UIImageView()
+    dateLabel = UILabel()
+    temperatureLabel = UILabel()
+    weatherLabel = UILabel()
+    let dashLabel: UILabel = UILabel()
+    descriptionLabel = UILabel()
+    
+    let labels: [UILabel] = [dateLabel, temperatureLabel, weatherLabel, dashLabel, descriptionLabel]
+    
+    labels.forEach { label in
+      label.textColor = .black
+      label.font = .preferredFont(forTextStyle: .body)
+      label.numberOfLines = 1
     }
     
-    required init?(coder: NSCoder) {
-        fatalError()
-    }
+    let weatherStackView: UIStackView = UIStackView(arrangedSubviews: [
+      weatherLabel,
+      dashLabel,
+      descriptionLabel
+    ])
     
-    override func prepareForReuse() {
-        super.prepareForReuse()
-        reset()
-    }
+    descriptionLabel.setContentHuggingPriority(.defaultLow,
+                                               for: .horizontal)
     
-    private func layViews() {
-        weatherIcon = UIImageView()
-        dateLabel = UILabel()
-        temperatureLabel = UILabel()
-        weatherLabel = UILabel()
-        let dashLabel: UILabel = UILabel()
-        descriptionLabel = UILabel()
-        
-        let labels: [UILabel] = [dateLabel, temperatureLabel, weatherLabel, dashLabel, descriptionLabel]
-        
-        labels.forEach { label in
-            label.textColor = .black
-            label.font = .preferredFont(forTextStyle: .body)
-            label.numberOfLines = 1
-        }
-        
-        let weatherStackView: UIStackView = UIStackView(arrangedSubviews: [
-            weatherLabel,
-            dashLabel,
-            descriptionLabel
-        ])
-        
-        descriptionLabel.setContentHuggingPriority(.defaultLow,
-                                                   for: .horizontal)
-        
-        weatherStackView.axis = .horizontal
-        weatherStackView.spacing = 8
-        weatherStackView.alignment = .center
-        weatherStackView.distribution = .fill
-        
-        
-        let verticalStackView: UIStackView = UIStackView(arrangedSubviews: [
-            dateLabel,
-            temperatureLabel,
-            weatherStackView
-        ])
-        
-        verticalStackView.axis = .vertical
-        verticalStackView.spacing = 8
-        verticalStackView.distribution = .fill
-        verticalStackView.alignment = .leading
-        
-        let contentsStackView: UIStackView = UIStackView(arrangedSubviews: [
-            weatherIcon,
-            verticalStackView
-        ])
-               
-        contentsStackView.axis = .horizontal
-        contentsStackView.spacing = 16
-        contentsStackView.alignment = .center
-        contentsStackView.distribution = .fill
-        contentsStackView.translatesAutoresizingMaskIntoConstraints = false
-        
-        contentView.addSubview(contentsStackView)
-                
-        NSLayoutConstraint.activate([
-            contentsStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
-            contentsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
-            contentsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
-            contentsStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
-            weatherIcon.widthAnchor.constraint(equalTo: weatherIcon.heightAnchor),
-            weatherIcon.widthAnchor.constraint(equalToConstant: 100)
-        ])
-    }
+    weatherStackView.axis = .horizontal
+    weatherStackView.spacing = 8
+    weatherStackView.alignment = .center
+    weatherStackView.distribution = .fill
     
-    private func reset() {
-        weatherIcon.image = UIImage(systemName: "arrow.down.circle.dotted")
-        dateLabel.text = "0000-00-00 00:00:00"
-        temperatureLabel.text = "00℃"
-        weatherLabel.text = "~~~"
-        descriptionLabel.text = "~~~~~"
-    }
+    
+    let verticalStackView: UIStackView = UIStackView(arrangedSubviews: [
+      dateLabel,
+      temperatureLabel,
+      weatherStackView
+    ])
+    
+    verticalStackView.axis = .vertical
+    verticalStackView.spacing = 8
+    verticalStackView.distribution = .fill
+    verticalStackView.alignment = .leading
+    
+    let contentsStackView: UIStackView = UIStackView(arrangedSubviews: [
+      weatherIcon,
+      verticalStackView
+    ])
+    
+    contentsStackView.axis = .horizontal
+    contentsStackView.spacing = 16
+    contentsStackView.alignment = .center
+    contentsStackView.distribution = .fill
+    contentsStackView.translatesAutoresizingMaskIntoConstraints = false
+    
+    contentView.addSubview(contentsStackView)
+    
+    NSLayoutConstraint.activate([
+      contentsStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
+      contentsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
+      contentsStackView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -16),
+      contentsStackView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -16),
+      weatherIcon.widthAnchor.constraint(equalTo: weatherIcon.heightAnchor),
+      weatherIcon.widthAnchor.constraint(equalToConstant: 100)
+    ])
+  }
+  
+  private func reset() {
+    weatherIcon.image = UIImage(systemName: "arrow.down.circle.dotted")
+    dateLabel.text = "0000-00-00 00:00:00"
+    temperatureLabel.text = "00℃"
+    weatherLabel.text = "~~~"
+    descriptionLabel.text = "~~~~~"
+  }
 }

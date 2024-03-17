00aaa624a5f36366cd4dc6680459fa62ede9edad
MinyoungKoo <rnalsdud1014@naver.com>
2024-03-18 00:17:18 +0900

    [Refactor] STEP1 - WeatherTableViewCell의 메서드 나누기 및 매직리터럴 제거
    layViews에서 모든 UI 컴포넌트 초기화 및 레이아웃을 설정하고 있기 때문에 이를 분리한다.
    reset 함수에서 사용하던 매직리터럴을 상수를 선언해 제거한다.

--- WeatherForecast/WeatherForecast/WeatherTableViewCell.swift
+++ WeatherForecast/WeatherForecast/WeatherTableViewCell.swift
@@ -12,9 +12,11 @@ class WeatherTableViewCell: UITableViewCell {
     var temperatureLabel: UILabel!
     var weatherLabel: UILabel!
     var descriptionLabel: UILabel!
+    var dashLabel: UILabel!
      
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
+        initialSetUp()
         layViews()
         reset()
     }
@@ -28,14 +30,16 @@ class WeatherTableViewCell: UITableViewCell {
         reset()
     }
     
-    private func layViews() {
+    private func initialSetUp() {
         weatherIcon = UIImageView()
         dateLabel = UILabel()
         temperatureLabel = UILabel()
         weatherLabel = UILabel()
-        let dashLabel: UILabel = UILabel()
         descriptionLabel = UILabel()
-        
+        dashLabel = UILabel()
+    }
+    
+    private func layLabels() {
         let labels: [UILabel] = [dateLabel, temperatureLabel, weatherLabel, dashLabel, descriptionLabel]
         
         labels.forEach { label in
@@ -44,20 +48,27 @@ class WeatherTableViewCell: UITableViewCell {
             label.numberOfLines = 1
         }
         
-        let weatherStackView: UIStackView = UIStackView(arrangedSubviews: [
+        descriptionLabel.setContentHuggingPriority(.defaultLow,
+                                                   for: .horizontal)
+    }
+    
+    private func weatherStackView() -> UIStackView {
+        let weatherStackView = UIStackView(arrangedSubviews: [
             weatherLabel,
             dashLabel,
             descriptionLabel
         ])
-        
-        descriptionLabel.setContentHuggingPriority(.defaultLow,
-                                                   for: .horizontal)
         
         weatherStackView.axis = .horizontal
         weatherStackView.spacing = 8
         weatherStackView.alignment = .center
         weatherStackView.distribution = .fill
         
+        return weatherStackView
+    }
+    
+    private func verticalStackView() -> UIStackView {
+        let weatherStackView = weatherStackView()
         
         let verticalStackView: UIStackView = UIStackView(arrangedSubviews: [
             dateLabel,
@@ -70,19 +81,30 @@ class WeatherTableViewCell: UITableViewCell {
         verticalStackView.distribution = .fill
         verticalStackView.alignment = .leading
         
+        return verticalStackView
+    }
+    
+    private func contentsStackView() -> UIStackView {
+        let verticalStackView = verticalStackView()
+        
         let contentsStackView: UIStackView = UIStackView(arrangedSubviews: [
             weatherIcon,
             verticalStackView
         ])
-               
+        
         contentsStackView.axis = .horizontal
         contentsStackView.spacing = 16
         contentsStackView.alignment = .center
         contentsStackView.distribution = .fill
         contentsStackView.translatesAutoresizingMaskIntoConstraints = false
-        
+               
+        return contentsStackView
+    }
+    
+    private func layContentsStackView() {
+        let contentsStackView = contentsStackView()
         contentView.addSubview(contentsStackView)
-                
+
         NSLayoutConstraint.activate([
             contentsStackView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 16),
             contentsStackView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 16),
@@ -93,11 +115,24 @@ class WeatherTableViewCell: UITableViewCell {
         ])
     }
     
-    private func reset() {
-        weatherIcon.image = UIImage(systemName: "arrow.down.circle.dotted")
-        dateLabel.text = "0000-00-00 00:00:00"
-        temperatureLabel.text = "00℃"
-        weatherLabel.text = "~~~"
-        descriptionLabel.text = "~~~~~"
+    private func layViews() {
+        layLabels()
+        layContentsStackView()
     }
+    
+    private func reset() {
+        weatherIcon.image = UIImage(systemName: Placeholder.weatherIcon)
+        dateLabel.text = Placeholder.dateLabel
+        temperatureLabel.text = Placeholder.temperatureLabel
+        weatherLabel.text = Placeholder.weatherLabel
+        descriptionLabel.text = Placeholder.descriptionLabel
+    }
+}
+
+enum Placeholder {
+    static let weatherIcon = "arrow.down.circle.dotted"
+    static let dateLabel = "0000-00-00 00:00:00"
+    static let temperatureLabel = "00℃"
+    static let weatherLabel = "~~~"
+    static let descriptionLabel = "~~~~~"
 }

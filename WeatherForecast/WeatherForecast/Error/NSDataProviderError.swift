0221d90291ba5fec20bfaefcdf03cb6faaef2510
honghoker <ekflzhftk@naver.com>
2024-03-18 01:33:11 +0900

    STEP 2: 단위 테스트를 위해 날씨 데이터를 제공하는 타입을 추상화하고 해당 타입을 의존하도록 변경

---
+++ WeatherForecast/WeatherForecast/Error/NSDataProviderError.swift
@@ -0,0 +1,12 @@
+//
+//  NSDataProviderError.swift
+//  WeatherForecast
+//
+//  Created by 홍은표 on 3/18/24.
+//
+
+import Foundation
+
+enum NSDataProviderError: Error {
+  case notFoundData
+}
--- WeatherForecast/WeatherForecast/Service/WeatherFetcherService.swift
+++ WeatherForecast/WeatherForecast/Service/WeatherFetcherService.swift
@@ -11,21 +11,25 @@ protocol WeatherFetcherServiceable {
   func execute() -> Result<WeatherJSON, WeatherFetcherError>
 }
 
-final class WeatherFetcherService: WeatherFetcherServiceable {
+final class WeatherFetcherService: WeatherFetcherServiceable, NSDataProviderProtocol {
   func execute() -> Result<WeatherJSON, WeatherFetcherError> {
     let jsonDecoder: JSONDecoder = .init()
     jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
     
-    guard let data = NSDataAsset(name: "weather")?.data else {
-      return .failure(.notFoundData)
-    }
+    let fetchResult = fetchWeatherData()
     
-    do {
-      let info: WeatherJSON = try jsonDecoder.decode(WeatherJSON.self, from: data)
-      return .success(info)
-    } catch {
-      print(error.localizedDescription)
-      return .failure(.failedToDecode)
+    switch fetchResult {
+    case .success(let data):
+      do {
+        let info: WeatherJSON = try jsonDecoder.decode(WeatherJSON.self, from: data)
+        return .success(info)
+      } catch {
+        print(error.localizedDescription)
+        return .failure(.failedToDecode)
+      }
+    case .failure(let error):
+      print(error)
+      return .failure(.notFoundData)
     }
   }
 }
